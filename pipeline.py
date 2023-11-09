0f12451c274b522e473bde790ff874e384ea906c
tbclements <tbclements@crimson.ua.edu>
2023-11-09 10:40:56 -0600

    testlab

--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.kernel_approximation import Nystroem
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import make_pipeline
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,8 +11,11 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
-# Average CV score on the training set was: 0.9318147687570321
-exported_pipeline = KNeighborsClassifier(n_neighbors=20, p=2, weights="uniform")
+# Average CV score on the training set was: 0.9316608041688873
+exported_pipeline = make_pipeline(
+    Nystroem(gamma=0.65, kernel="laplacian", n_components=2),
+    GradientBoostingClassifier(learning_rate=0.1, max_depth=8, max_features=0.7000000000000001, min_samples_leaf=10, min_samples_split=11, n_estimators=100, subsample=0.1)
+)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
